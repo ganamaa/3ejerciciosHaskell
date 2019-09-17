acd05a2f0b2f4aad87f83ff6a286812af8ed9759
ganamaa <54151757+ganamaa@users.noreply.github.com>
2019-09-17 12:09:11 -0500

    Add files via upload

---
+++ listas2.hs
@@ -0,0 +1,24 @@
+mayor:: [Int]->Int
+mayor [x] = x
+mayor (x:xs)
+ | x > mayor(xs) = x
+ | otherwise = mayor(xs)
+
+mayor_listas::[[Int]]->[Int] -- ejercicio numero1
+mayor_listas x = [mayor z | z <- x]
+
+menor:: [Int]->Int
+menor [x] = x
+menor (x:xs)
+ | x < menor(xs) = x
+ | otherwise = menor(xs)
+ 
+multiplos:: [Int]->Int->Int
+multiplos lista y= length [x | x <- lista, mod x y ==0, x>y || x<y]
+
+divisibles::[Int]->Int->Int
+divisibles lista y= length [x | x <- lista, mod y x ==0, x>y || x<y]
+
+listaTuplas:: [[Int]]->[(Int,Int,Int,Int)] -- ejercicio numero 3
+listaTuplas x = [(menor z,mayor z,multiplos z y,divisibles z a)|z <- x, let y = menor z, let a = mayor z]
+
