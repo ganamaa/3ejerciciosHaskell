bdabe3b02ecc736a5651b8a25f2af05b718fedb3
ganamaa <54151757+ganamaa@users.noreply.github.com>
2019-09-17 13:05:30 -0500

    Update listas2.hs

--- listas2.hs
+++ listas2.hs
@@ -22,3 +22,10 @@ divisibles lista y= length [x | x <- lista, mod y x ==0, x>y || x<y]
 listaTuplas:: [[Int]]->[(Int,Int,Int,Int)] -- ejercicio numero 3
 listaTuplas x = [(menor z,mayor z,multiplos z y,divisibles z a)|z <- x, let y = menor z, let a = mayor z]
 
+prom :: [Int]->Int
+prom [] = 0
+prom xs = div sum xs length xs 
+
+prom_de_lista:: [[Int]]->[Int]
+prom_de_lista x = [prom z | z <- x]
+
